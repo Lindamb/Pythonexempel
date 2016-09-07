1776b6e53e31896e4111f034003359c01b5f737c
Lindamb <linda@Lindas-MacBook-Pro.local>
2016-09-07 14:41:01 +0200

    Klass med konstruktor
    Lade till init och testade att adda en produkt.

--- pythonexempel.py
+++ pythonexempel.py
@@ -1,18 +1,17 @@
 class Product:
-	price = 0
-	count = 0
-	tax = 0
-	def price_with_tax(self):
-		return self.price * self.count * self.tax
+		price = 0
+		count = 0
+		tax = 1
+		def __init__(self, price, count, tax):
+			self.price = price
+			self.count = count
+			self.tax = tax
+		def price_with_tax(self):
+			return self.price * self.count * self.tax
+		
+robot = Product(price= 900, count= 2, tax= 1.25)
+book = Product(price= 100, count= 1, tax= 1.06)
+husvagn = Product(price= 1000, count=1, tax= 1.25)
+print(robot.price_with_tax() + book.price_with_tax() + husvagn.price_with_tax())
 
-robot = Product()
-robot.price = 900
-robot.count = 2
-robot.tax = 1.25
 
-book = Product()
-book.price = 100
-book.count = 1
-book.tax = 1.06	
-
-print(robot.price_with_tax() + book.price_with_tax())
